3fe248c03030cfc58b63d29fc4d717520c56370b
Duong Nguyen <dnx04.gg@gmail.com>
2022-06-05 22:18:42 +0700

    adding assert for union_find

--- data-structure/union_find.hpp
+++ data-structure/union_find.hpp
@@ -3,6 +3,8 @@ struct uf {
   uf(int _n) : n(_n), p(_n, -1) {}
 
   int merge(int a, int b) {
+    assert(0 <= a && a < n);
+    assert(0 <= b && b < n);
     int x = head(a), y = head(b);
     if (x == y) return x;
     if (-p[x] < -p[y]) swap(x, y);
@@ -11,9 +13,14 @@ struct uf {
     return x;
   }
 
-  bool same(int a, int b) { return head(a) == head(b); }
+  bool same(int a, int b) {
+    assert(0 <= a && a < n);
+    assert(0 <= b && b < n);
+    return head(a) == head(b);
+  }
 
   int head(int a) {
+    assert(0 <= a && a < n);
     if (p[a] < 0) return a;
     return p[a] = head(p[a]);
   }
