28786d23d31fb1c2cf607e698626491e9b8545c6
Duong Nguyen <dnx04.gg@gmail.com>
2025-11-15 17:05:59 +0700

    revive the project

--- tests/Vertex_Add_Path_Sum.test.cpp
+++ tests/Vertex_Add_Path_Sum.test.cpp
@@ -20,20 +20,20 @@ signed main() {
     g[u].eb(v), g[v].eb(u);
   }
   auto hld = HLD(g);
-  for (int i = 0; i < n; ++i) fw.add(hld.pos(i) + 1, a[i]);
+  for (int i = 0; i < n; ++i) fw.add(hld.idx(i).first + 1, a[i]);
   while (q--) {
     int cmd;
     cin >> cmd;
     if (cmd == 0) {
       int p, x;
       cin >> p >> x;
-      fw.add(hld.pos(p) + 1, x);
+      fw.add(hld.idx(p).first + 1, x);
     } else {
       int u, v;
       cin >> u >> v;
       ll res = 0;
-      hld.path_query(u, v, [&](const int& u, const int& v) {
-        res += fw.sum(u + 1, v + 1);
+      hld.path_query(u, v, true, [&](const int& u, const int& v) {
+        res += fw.sum(u + 1, v);
       });
       cout << res << '\n';
     }
